5d67daff7cd0c320c241e6eed0ba501c5fe30a87
sunshiheima <sunshiheima@qq.com>
2021-06-09 00:48:19 +0800

    react 学习

--- myreactapp/src/index.js
+++ myreactapp/src/index.js
@@ -13,22 +13,61 @@ import reactDom from 'react-dom';
 // );
 class Hello extends React.Component{
  state={
-   name:0,
+   list:[],
+    formData:{
+      name:"",
+      content:"",
+    }
  }
  onchange=(e)=>{
   this.setState({
-    [e.target.name]:e.target.value || e.target.checked,
+    formData:{
+      ...this.state.formData,[e.target.name]:e.target.value || e.target.checked,
+    }
   }
  )
+}
+sunmit=()=>{
+  this.setState({
+    list:[this.state.formData,...this.state.list]
+  })
 }
   render(){
     return (
       <div>
-        <label><input type="text" name="name" onChange={this.onchange}/></label>
+        <label>姓名：<input type="text" placeholder="请输入" name="name" onChange={this.onchange}/></label>
+        <label>内容：</label>
+        <br/>
+        <textarea rows="5" cols="100" placeholder="请输入" name="content" onChange={this.onchange}> </textarea>
         <label>{this.state.name}</label>
+        <div>
+          <button type="button" onClick={this.sunmit}>提交</button>
+        </div>
+        <div>
+          {
+            this.state.list.map(item=>{
+              return (
+                <div>
+                  <div><div>姓名：</div>{item.name}</div>
+                  <div><div>内容：</div>{item.content}</div>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
 }
+// class SubDom extends React.Component{
+//   constructor(props){
+
+//   }
+//   render(){
+//     return(
+//       <p></p>
+//     )
+//   }
+// }
 reactDom.render(<Hello/>, document.getElementById("root"))
 reportWebVitals();
