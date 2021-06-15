acd2e2ceccf545a016abe421e6291599a0c003b6
sunshiheima <sunshiheima@qq.com>
2021-06-15 18:35:17 +0800

    框架修改

--- uniApp/utils/tool.js
+++ uniApp/utils/tool.js
@@ -1,28 +1,59 @@
+/*
+ * @Author: sunshiheima 
+ * @Date: 2021-06-15 15:56:33 
+ * @Last Modified by: sunshiheima
+ * @Last Modified time: 2021-06-15 15:57:43
+ * 项目公共方法
+ */
 import Vue from "vue";
 /* 显示提示 */
 export const Toast = class Toast {
 	constructor(timeOut) {
 		this.timeOut = timeOut;
 	}
-	static showToast(title = "成功", callBack, data = null) {
+	static showToast(title = "成功", callBack, data = {}) {
 		uni.hideToast();
-		uni.showToast({
-			title,
-			icon: (data && data.icon) ? data.icon : "succes",
-			duration: (data && data.ctimeOut) ? data.duration : 1500,
-			image: (data && data.image) ? data.image : null,
-			mask: (data && data.mask) ? data.mask : false,
-			position: (data && data.position) ? data.position : "center",
+		let obj = Object.assign({ title, }, data, {
 			complete() {
 				if (callBack) {
 					Toast.timeOut = setTimeout(() => {
-						callBack();
+						callBack(uni);
 						clearTimeout(Toast.timeOut)
 					}, 1500)
 				}
 
 			}
 		})
+		uni.showToast(obj);
 	}
 }
 Vue.prototype.$showToast = Toast.showToast;
+/* 路由传参封装 */
+export const urlOptions = (object, isForm) => {
+	let str = "?"
+	let bg = false;
+	for (let key in object) {
+		if (isForm) {
+			for (let k in object[key]) {
+				if (k == "value") {
+					if (bg) {
+						str += `&${key}=${object[key][k]}`;
+					} else {
+						bg = true;
+						str += `${key}=${object[key][k]}`;
+					}
+				}
+			}
+		} else {
+			if (bg) {
+				str += `&${key}=${object[key]}`;
+			} else {
+				bg = true;
+				str += `${key}=${object[key]}`;
+			}
+		}
+
+	}
+	return str;
+}
+Vue.prototype.$urlOptions = urlOptions;
