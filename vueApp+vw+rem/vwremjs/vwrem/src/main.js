54d5f3e0833c21852855718cc23a4f5df5e67fd0
sunshiheima <sunshiheima@qq.com>
2022-07-04 09:58:26 +0800

    正则学习

--- vueApp+vw+rem/vwremjs/vwrem/src/main.js
+++ vueApp+vw+rem/vwremjs/vwrem/src/main.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-21 09:58:54
+ * @LastEditTime: 2021-11-26 16:18:13
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vwrem_appd:\sunshiheima\myStudy\MyPoject\vueApp+vw+rem\vwremjs\vwrem\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
